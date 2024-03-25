4c7af205b13dbab983162535330a4b5c6e944939
Jean-Philippe BLOND <jpb.blond@gmail.com>
2024-03-25 13:53:48 +0100

    display amountTaxIncluded & loanPrice

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
         <input type="float"  v-model="rate" class="form-control" id="rate" placeholder="Taux du prêt" />
       </div>
       <div class="input-group mb-3">
+        <span class="input-group-text" id="tax">Type</span>
       <select v-model="tax" class="form-select" aria-label="Default select example">
         <option value="8" selected>Ancien (8% de Frais de notaire)</option>
         <option value="3">Neuf (3% de Frais de notaire)</option>
@@ -32,8 +33,10 @@
         <button @click="calculate" class="btn btn-primary" id="submit">Calculer !</button>
       </div>
     </div>
+      <p v-if="amountTaxIncluded !== undefined">Prix FDN inclus: {{amountTaxIncluded.toFixed(2)}} €</p>
+      <p v-if="loanPrice !== undefined">Count du crédit: {{loanPrice.toFixed(2)}} €</p>
       <p v-if="monthlyRent !== undefined"><strong>Mensualité: {{monthlyRent.toFixed(2)}} €</strong></p>
-  <p v-if="typeof monthlydebtRate === 'number'" :style="{color: debtColor}"><strong>Endettement: {{monthlydebtRate.toFixed(2)}}%</strong></p>
+      <p v-if="typeof monthlydebtRate === 'number'" :style="{color: debtColor}"><strong>Endettement: {{monthlydebtRate.toFixed(2)}}%</strong></p>
 </template>
 
 <script>
@@ -49,7 +52,9 @@ export default {
       tax: 8,
       monthlySalary: undefined,
       monthlydebtRate: undefined,
-      debtColor: undefined
+      debtColor: undefined,
+      loanPrice: undefined,
+      amountTaxIncluded: undefined
   };
 },
   methods: {
@@ -57,13 +62,12 @@ export default {
       console.log({tax: this.tax})
       const percentage = (100 + Number(this.tax)) / 100;
       console.log({appliedPercentage: percentage})
-      const amountTaxIncluded = (this.rawAmount ?? 0) * percentage;
-      console.log({amountTaxIncluded})
+      this.amountTaxIncluded = (this.rawAmount ?? 0) * percentage;
 
       const monthlyRate = this.rate / 12 / 100;
       const numberOfMonths = this.duration * 12;
 
-      const amountToLoan = amountTaxIncluded - (this.contribution ?? 0);
+      const amountToLoan = this.amountTaxIncluded - (this.contribution ?? 0);
 
       const firstPart = amountToLoan * monthlyRate;
       const secondPart = Math.pow((1 + monthlyRate), (-1 * numberOfMonths));
@@ -82,6 +86,8 @@ export default {
       } else {
         this.debtColor = 'red';
       }
+
+      this.loanPrice = (Number(this.monthlyRent) * numberOfMonths) - amountToLoan;
     }
   },
   watch: {
